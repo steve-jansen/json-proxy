04029663935bc8863a8d93cc83afb4bf29e535e1
Steve Jansen <stevejansen_github@icloud.com>
2015-03-18 21:07:00 -0400

    update middleware example to express 4.x

--- examples/middleware/index.js
+++ examples/middleware/index.js
@@ -1,20 +1,19 @@
 var express = require('express'),
+    morgan = require('morgan'),  
     proxy = require('../../lib/proxy');
 
 var app = express();
 
-app.configure(function() {
-  app.use(express.favicon(false));
-  app.use(express.logger('dev'));
-  app.use(proxy.initialize({
-    proxy: {
-      'forward': {
-        '/channel': 'http://www.youtube.com'
-      }
+app.use(morgan('dev'));
+app.use(proxy.initialize({
+  proxy: {
+    'forward': {
+      '/channel': 'https://www.youtube.com'
     }
-  }));
-  app.use(express.static(__dirname));
-});
+  }
+}));
+
+app.use(express.static(__dirname));
 
 app.listen(5000);
 console.log('listening on http://localhost:5000');
