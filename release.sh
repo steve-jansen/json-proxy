b60b7d726eef8ec39c05fcb1f5b332056062b475
Steve Jansen <stevejansen_github@icloud.com>
2015-05-23 06:11:58 -0400

    Revert git tagging with travis

--- release.sh
+++ release.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+APP_TAG="v`node -p 'require("./package.json").version'`"
+GIT_COMMIT=`git rev-parse HEAD`
+
+# see http://docs.travis-ci.com/user/originment/custom/
+echo $0: configuring git
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
-APP_TAG="v`node -p 'require("package.json").version'`"
+echo $0: git reseting
+git reset --hard HEAD
 
-echo $0: tagging commit ${TRAVIS_COMMIT} as ${APP_TAG}
-git tag "${APP_TAG}" "${TRAVIS_COMMIT}"
-
-echo $0: updating release branch to commit ${TRAVIS_COMMIT}
-git branch -f release "${TRAVIS_COMMIT}"
-
-echo $0: pushing tags and release branch
-git push -q origin "${APP_TAG}"
+echo $0: tagging commit ${GIT_COMMIT} as ${APP_TAG}
+git tag "${APP_TAG}" "${GIT_COMMIT}"
 
 echo $0: version bumping
-npm version minor -m "Version bump by Travis CI"
-git push -q origin master
+npm version patch -m 'Version bump to %s'
+
+echo $0: pushing tags and master
+git push -q origin master --tags
 
