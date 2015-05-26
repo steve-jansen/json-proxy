e1823727ee3feb3f246e43803c0a254272e7b32e
Steve Jansen <stevejansen_github@icloud.com>
2015-05-26 06:51:37 -0400

    Disable `npm version` tagging

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ echo $0: tagging commit ${GIT_COMMIT} as ${APP_TAG}
 git tag "${APP_TAG}" "${GIT_COMMIT}"
 
 echo $0: version bumping
-npm version patch -m 'Version bump to %s'
+npm version patch -m 'Version bump to %s' --no-git-tag-version
 
 echo $0: pushing tags and master
 git push -q origin master --tags
